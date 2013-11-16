9b846de458f870496f2c9417a5b0f886ec510913
Robert Jacob <xperimental@solidproject.de>
2013-11-16 18:17:54 +0100

    Customization for GTUG.

--- js/config.js
+++ js/config.js
@@ -1,16 +1,16 @@
 boomerang.factory('Config',function(){
     return {
         //modify these
-        'name'          : 'GDG Fresno',
-        'id'            : '114769570436363155784',
-        'google_api'    : 'AIzaSyDPjRfTjr-X-FB0jRSf06_7rHAvu9oQ3To',
-        'pwa_id'        : '5846413253595166705', //picasa web album id, must belong to google+ id above
+        'name'          : 'GTUG Lake Constance',
+        'id'            : '102863795915305230399',
+        'google_api'    : 'AIzaSyCoY8fJm9CEc4p5Pj4Cwj9MqU-d3O3sM7I',
+        'pwa_id'        : '5884887694290374801', //picasa web album id, must belong to google+ id above
         'cover' : {
-            title : 'DevFest Fresno',
-            subtitle : 'The largest Google Developer Conference in the Central Valley',
+            title : 'GTUG Lake Constance',
+            subtitle : 'Let\'s talk about software and technology...',
             button : {
-                text : 'Register',
-                url : 'https://devfestfresno.eventbrite.com'
+                text : 'Join',
+                url : 'https://plus.google.com/102863795915305230399'
             }
         }
     }
