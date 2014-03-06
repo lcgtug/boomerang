7af87041ad4866c1cd7895d1df9b50a1c6bf45a0
Robert Jacob <xperimental@solidproject.de>
2014-03-06 23:59:35 +0100

    Prepared website for new GDG name.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 boomerang.factory('Config', function () {
     return {
         //modify these
-        'name'          : 'GTUG Lake Constance',
+        'name'          : 'GDG Bodensee',
         'id'            : '102863795915305230399',
         'google_api'    : 'AIzaSyCoY8fJm9CEc4p5Pj4Cwj9MqU-d3O3sM7I',
         'pwa_id'        : '5884887694290374801', //picasa web album id, must belong to google+ id above
         'cover' : {
-            title : 'GTUG Lake Constance',
+            title : 'GDG Bodensee',
             subtitle : 'Let\'s talk about software and technology...',
             button : {
                 text : 'Join',
