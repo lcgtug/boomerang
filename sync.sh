237697148b75ea160cff2bec62f5a5f1fa215df4
Robert Jacob <xperimental@solidproject.de>
2015-05-06 23:07:00 +0200

    Update sync script.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd sync -v --exclude-from=sync-excludes --acl-public . s3://gdg-lc-web
+s3cmd sync -v --exclude-from=sync-excludes --acl-public --delete-removed . s3://gdg-lc-web
 
