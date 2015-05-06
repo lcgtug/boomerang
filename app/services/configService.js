0f28ca757b14e693f049732d107139a88a2a1256
Robert Jacob <xperimental@solidproject.de>
2015-05-06 21:54:41 +0200

    Move configuration to new location.

--- app/services/configService.js
+++ app/services/configService.js
@@ -2,22 +2,22 @@ angular.module('gdgXBoomerang')
 .factory('Config', function () {
     return {
         // TODO Modify these to configure your app
-        'name'          : 'GDG Space Coast',
-        'id'            : '103959793061819610212',
-        'googleApi'     : '<insert your API key here>',
-        'pwaId'         : '5915725140705884785', // Picasa Web Album id, must belong to Google+ id above
-        'domain'        : 'http://www.gdgspacecoast.org',
-        'twitter'       : 'gdgspacecoast',
-        'facebook'      : 'gdgspacecoast',
-        'meetup'        : 'gdgspacecoast',
+        'name'          : 'GDG Bodensee',
+        'id'            : '102863795915305230399',
+        'googleApi'     : 'AIzaSyCoY8fJm9CEc4p5Pj4Cwj9MqU-d3O3sM7I',
+        'pwaId'         : '5884887694290374801', // Picasa Web Album id, must belong to Google+ id above
+        'domain'        : 'http://www.gdg-bodensee.de',
+        'twitter'       : 'lcgtug',
+        'facebook'      : '',
+        'meetup'        : 'GDG-Bodensee',
         // Change to 'EEEE, MMMM d, y - H:mm' for 24 hour time format.
-        'dateFormat'    : 'EEEE, MMMM d, y - h:mm a',
+        'dateFormat'    : 'EEEE, MMMM d, y - H:mm',
         'cover' : {
-            title: 'Worldwide GDG Events',
-            subtitle: 'Directory of developer events organized by tags and displayed on a global map.',
-            button: {
-                text: 'Find local events',
-                url: 'http://gdg.events/'
+            title : 'GDG Bodensee',
+            subtitle : 'Let\'s talk about software and technology...',
+            button : {
+                text : 'Join',
+                url : 'https://plus.google.com/102863795915305230399'
             }
         }
         // To update the snippet which is used for sharing, see the TODO in the index.html.
